bfda8f4ff0f293838b7aeee49183121397e09a9d
Gavin Montgomery <gavinmontgomery@cedarville.edu>
2024-03-13 15:21:23 -0400

    Goofy ahh commit message

---
+++ goofy_ahh_script.py
@@ -0,0 +1,9 @@
+import webbrowser as smeebeefreebee
+import base64 as bleebeedeebee
+
+def thfguhihgjgcgjkl():
+    e_safgrhtytuyutkdyjrsthrg = "aHR0cHM6Ly95b3V0dS5iZS9kUXc0dzlXZ1hjUQ=="
+    d_asfsgrhtjyktukyjthrseag = bleebeedeebee.b64decode(e_safgrhtytuyutkdyjrsthrg)
+    smeebeefreebee.open(d_asfsgrhtjyktukyjthrseag.decode())
+
+thfguhihgjgcgjkl()
